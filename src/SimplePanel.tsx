9da4d99a3a40acca6bb6bf69ebe7acc6d896ebb9
Chris West <westc@live.com>
2021-11-30 15:46:34 -0400

    Remove the code from default template.

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -10,13 +10,14 @@ import { ColDef } from 'ag-grid-community';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
+const theme = useTheme();
+
 function createFlagImg(flag: string) {
   return `<img border="0" width="15" height="10" src="https://flags.fmcdn.net/data/flags/mini/${flag}.png"/>`;
 }
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   try {
-    const theme = useTheme();
     const styles = getStyles();
 
     const defaultColDef: ColDef = {
@@ -180,31 +181,6 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
           `
         )}
       >
-        {/* <svg
-          className={styles.svg}
-          width={width}
-          height={height}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-        >
-          <g>
-            <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-          </g>
-        </svg>
-
-        <div className={styles.textBox}>
-          {options.showSeriesCount && (
-            <div
-              className={css`
-                font-size: ${theme.typography.size[options.seriesCountSize]};
-              `}
-            >
-              Number of series: {data.series.length}
-            </div>
-          )}
-          <div>Text option value: {options.text}</div>
-        </div> */}
         <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
           <AgGridReact
             rowData={rowData}
@@ -229,17 +205,6 @@ const getStyles = stylesFactory(() => {
   return {
     wrapper: css`
       position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
     `,
   };
 });
